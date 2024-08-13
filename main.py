978c8871f48b4ce8b7906663717b6db4534d7b9b
Juan Ernesto Sierra <juersierra@gmail.com>
2024-08-13 17:34:58 -0300

    added arwork management

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-# from pydantic import BaseModel, Field
-from .models import Artist
-# from .models import Artwork
+from .models import *
 
 app = FastAPI()
-
-
 
 @app.get("/")
 def index():
@@ -15,13 +11,12 @@ def index():
 def returnId(id: int):
     return {"id": id}
 
-class Artist(BaseModel):
-    id: int | None = Field(default=None)
-    name: str
-    pseudo: str
-    description: str
-
 @app.post("/artist")
 def create_artist(artist:Artist):
     artista = artist
+    return artista
+
+@app.post("/artwork")
+def create_artwork(artwork:Artwork):
+    artista = artwork
     return artista
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 from .artists import *
+from .artwork import *
